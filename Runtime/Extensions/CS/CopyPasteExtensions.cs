432ac37162b17102f4aac70ab2f2f35f14ceabee
Atomiz <tytunipex@gmail.com>
2026-05-04 13:45:42 +0300

    Fixed CopyPasteExtensions #if

--- Runtime/Extensions/CS/CopyPasteExtensions.cs
+++ Runtime/Extensions/CS/CopyPasteExtensions.cs
@@ -1,26 +1,36 @@
-﻿#if DEVELOPMENT_ESSENTIALS_RUNTIME_NEWTONSOFT_JSON && UNITY_EDITOR && !SIMULATE_BUILD
-using Newtonsoft.Json;
+﻿#if UNITY_EDITOR && !SIMULATE_BUILD
 using UnityEditor;
 using UnityEngine;
+#if DEVELOPMENT_ESSENTIALS_RUNTIME_NEWTONSOFT_JSON
+using Newtonsoft.Json;
+#endif
 
 namespace DevelopmentEssentials.Extensions.CS {
 
     public static class CopyPasteExtensions {
 
         public static void CopyObjToClipboard<T>(this T t) {
+#if DEVELOPMENT_ESSENTIALS_RUNTIME_NEWTONSOFT_JSON
             EditorGUIUtility.systemCopyBuffer = t is Object obj
                 ? JsonUtility.ToJson(obj, true)
                 : JsonConvert.SerializeObject(t, Formatting.Indented);
+#else
+            EditorGUIUtility.systemCopyBuffer = JsonUtility.ToJson(t, true);
+#endif
 
             if (t is ICopyable copyable)
                 copyable.OnCopy(EditorGUIUtility.systemCopyBuffer);
         }
 
         public static void PasteObjFromClipboard<T>(this T t) {
+#if DEVELOPMENT_ESSENTIALS_RUNTIME_NEWTONSOFT_JSON
             if (t is Object obj)
                 JsonUtility.FromJsonOverwrite(EditorGUIUtility.systemCopyBuffer, obj);
             else
                 JsonConvert.PopulateObject(EditorGUIUtility.systemCopyBuffer, t);
+#else
+            JsonUtility.FromJsonOverwrite(EditorGUIUtility.systemCopyBuffer, t);
+#endif
 
             if (t is ICopyable copyable)
                 copyable.OnPaste();
