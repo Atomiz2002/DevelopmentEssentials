54fbda2c08182ed58a65af810ec5c050b06bcfff
Atomiz <tytunipex@gmail.com>
2026-06-12 00:14:33 +0300

    MethodInfo.SafeInvoke

---
+++ Runtime/Extensions/CS/MethodInfoExtensions.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CS0168 // Variable is declared but never used
+using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace DevelopmentEssentials.Extensions.CS {
+
+    public static class MethodInfoExtensions {
+
+        public static void SafeInvoke(this MethodInfo methodInfo, object obj, object[] parameters) {
+            try {
+                methodInfo?.Invoke(obj, parameters);
+            }
+            catch (Exception e) {
+#if ENABLE_LOGS
+                e.LogEx();
+#endif
+            }
+        }
+
+        public static void SafeInvoke(this MethodInfo methodInfo, object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture) {
+            try {
+                methodInfo?.Invoke(obj, invokeAttr, binder, parameters, culture);
+            }
+            catch (Exception e) {
+#if ENABLE_LOGS
+                e.LogEx();
+#endif
+            }
+        }
+
+    }
+
+}
